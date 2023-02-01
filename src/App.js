f9e2f870c943c2fa04cbf61ea4b8ec08aee85f14
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-02-01 22:02:58 +0200

    done test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+
+import { data, strengthPassword } from "./data";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [strenthColorPassword, setStrenthColorPassword] = useState([]);
+
+  const inputChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+  };
+
+  const chekPaswword = () => {
+    let res = {};
+    data.arrNum.forEach((num) => {
+      if (password.includes(num)) {
+        res.hasNumber = true;
+        return;
+      }
+    });
+    data.arrLetters.forEach((num) => {
+      if (password.includes(num)) {
+        res.hasLetters = true;
+        return;
+      }
+    });
+    data.arrSymb.forEach((num) => {
+      if (password.includes(num)) {
+        res.hasSymbols = true;
+        return;
+      }
+    });
+    return res;
+  };
+
+  const {
+    hasLetters = false,
+    hasNumber = false,
+    hasSymbols = false,
+  } = chekPaswword();
+
+  useEffect(() => {
+    if (!password.length) {
+      setStrenthColorPassword(strengthPassword.empty);
+    } else if (password.length < "8") {
+      setStrenthColorPassword(strengthPassword.lessEightSymbol);
+    } else if (password.length >= 8 && hasNumber && !hasLetters & !hasSymbols) {
+      setStrenthColorPassword(strengthPassword.easy);
+    } else if (password.length >= 8 && !hasNumber && hasLetters & !hasSymbols) {
+      setStrenthColorPassword(strengthPassword.easy);
+    } else if (password.length >= 8 && !hasNumber && !hasLetters & hasSymbols) {
+      setStrenthColorPassword(strengthPassword.easy);
+    } else if (password.length >= 8 && hasNumber && hasLetters & !hasSymbols) {
+      setStrenthColorPassword(strengthPassword.medium);
+    } else if (password.length >= 8 && hasNumber && !hasLetters & hasSymbols) {
+      setStrenthColorPassword(strengthPassword.medium);
+    } else if (password.length >= 8 && !hasNumber && hasLetters & hasSymbols) {
+      setStrenthColorPassword(strengthPassword.medium);
+    } else if (password.length >= 8 && hasNumber && hasLetters & hasSymbols) {
+      setStrenthColorPassword(strengthPassword.strong);
+    }
+  }, [hasLetters, hasNumber, hasSymbols, password.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div>
+          <h1>Test password</h1>
+        </div>
+        <div className="Main-container">
+          <input
+            value={password}
+            onChange={inputChange}
+            type={"password"}
+            style={{ width: "95%" }}
+          ></input>
+          <div className="Strength-container">
+            <span
+              className="Strength"
+              style={{ backgroundColor: `${strenthColorPassword[0]}` }}
+            ></span>
+            <span
+              className="Strength"
+              style={{ backgroundColor: `${strenthColorPassword[1]}` }}
+            ></span>
+            <span
+              className="Strength"
+              style={{ backgroundColor: `${strenthColorPassword[2]}` }}
+            ></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
